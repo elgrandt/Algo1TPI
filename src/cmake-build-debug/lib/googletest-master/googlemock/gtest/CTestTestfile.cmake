23516c7bb9a1e91bf51064fe6efdb8ac5e208fd4
Dylan Tasat <dtasat@dc.uba.ar>
2017-10-29 15:52:27 -0300

    Deleted ide folders

--- src/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Diego/Documents/Facultad/Algoritmos/Labo/TP_Implementacion/salaDeReuniones_TPI/src/lib/googletest-master/googletest
-# Build directory: C:/Users/Diego/Documents/Facultad/Algoritmos/Labo/TP_Implementacion/salaDeReuniones_TPI/src/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
